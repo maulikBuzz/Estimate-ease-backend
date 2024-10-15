49866af12800d84550e4994d65f00d7d4518ed79
maulikBuzz <maulik.buzz@gmail.com>
2024-10-15 12:05:15 +0530

    some change in pdf generate

--- app/services/estimateService.js
+++ app/services/estimateService.js
@@ -932,6 +932,36 @@ const generatePdf = async ({ user_customer_id, user_id }) => {
 
         html = html.replace('{{#quotationItems}}', itemsHtml);
 
+      const html1=   `<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  margin-left: 200px;
+  background: #5d9ab2 url("img_tree.png") no-repeat top left;
+}
+
+.center_div {
+  border: 1px solid gray;
+  margin-left: auto;
+  margin-right: auto;
+  width: 90%;
+  background-color: #d0f0f6;
+  text-align: left;
+  padding: 8px;
+}
+</style>
+</head>
+<body>
+
+<div class="center_div">
+  <h1>Hello World!</h1>
+  <p>This example contains some advanced CSS methods you may not have learned yet. But, we will explain these methods in a later chapter in the tutorial.</p>
+</div>
+
+</body>
+</html>`
+
         const args = [
             '--allow-running-insecure-content',
             '--autoplay-policy=user-gesture-required',
@@ -980,7 +1010,7 @@ const generatePdf = async ({ user_customer_id, user_id }) => {
 
             const loaded = page.waitForNavigation({ waitUntil: 'networkidle0', timeout: 0 });
 
-            await page.setContent(html);
+            await page.setContent(html1);
             await loaded;
             const pdf = await page.pdf({
                 format: 'A4',
