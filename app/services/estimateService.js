0d284145590d64e726620c6620f6ce419bf736ad
maulikBuzz <maulik.buzz@gmail.com>
2024-10-16 17:39:46 +0530

    set pdf file

--- app/services/estimateService.js
+++ app/services/estimateService.js
@@ -936,34 +936,34 @@ const generatePdf = async ({ user_customer_id, user_id }) => {
         html = html.replace('{{#quotationItems}}', itemsHtml); 
 
         const html1 = `<!DOCTYPE html>
-// <html>
-// <head>
-// <style>
-// body {
-//   margin-left: 200px;
-//   background: #5d9ab2 url("img_tree.png") no-repeat top left;
-// }
+                 <html>
+                 <head>
+                 <style>
+                 body {
+                   margin-left: 200px;
+                   background: #5d9ab2 url("img_tree.png") no-repeat top left;
+                 }
 
-// .center_div {
-//   border: 1px solid gray;
-//   margin-left: auto;
-//   margin-right: auto;
-//   width: 90%;
-//   background-color: #d0f0f6;
-//   text-align: left;
-//   padding: 8px;
-// }
-// </style>
-// </head>
-// <body>
+                 .center_div {
+                   border: 1px solid gray;
+                   margin-left: auto;
+                   margin-right: auto;
+                   width: 90%;
+                   background-color: #d0f0f6;
+                   text-align: left;
+                   padding: 8px;
+                 }
+                 </style>
+                 </head>
+                 <body>
 
-// <div class="center_div">
-//   <h1>Hello World!</h1>
-//   <p>This example contains some advanced CSS methods you may not have learned yet. But, we will explain these methods in a later chapter in the tutorial.</p>
-// </div>
+                 <div class="center_div">
+                   <h1>Hello World!</h1>
+                   <p>This example contains some advanced CSS methods you may not have learned yet. But, we will explain these methods in a later chapter in the tutorial.</p>
+                 </div>
 
-// </body>
-// </html>`
+                 </body>
+                 </html>`
         
 
         try {
